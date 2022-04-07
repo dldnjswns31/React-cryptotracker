b38f092e48abd3627d326fb455cd794f460d1b7d
dldnjswns31 <dldnjswns32@gmail.com>
2022-04-08 04:07:27 +0900

    prepare to make the coin tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div>hello</div>;
+  const [username, setUsername] = useState("");
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setUsername(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`Hello ${username}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={username}
+          type="text"
+          placeholder="Username"
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
